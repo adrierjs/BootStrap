8ab40631e884b1bd36ea0cb5bd033528cfd07e43
adrierjs <adrierj@gmail.com>
2021-10-18 18:14:22 -0300

    req_com_url_requisitada

--- nodejs/MongoDB&NodeJS/noticias.js
+++ nodejs/MongoDB&NodeJS/noticias.js
@@ -1,6 +1,17 @@
-var http = require('http'); // acrencentar algo coisa
+var http = require('http'); // acrencentar algo coisa, criando a biblioteca
 
 var server = http.createServer(function(req, res){
-    res.end("<html><body>Portal de notícias</body><\html>");
+
+    var categoria = req.url;
+    if (categoria == '/tecnologia'){
+        res.end("<html><body>Noticias de tecnologias</body><\html>");
+    } else if (categoria == '/moda'){
+        res.end("<html><body>Noticias de moda</body><\html>");
+    } else if (categoria == '/beleza'){
+        res.end("<html><body>Noticias de beleza</body><\html>");
+    } else{
+         res.end("<html><body>Portal de noticia</body><\html>");
+    }
 }); //criar servidor 
 server.listen(3000); //escutar em uma porta
+//Ctrl + Alt + N para executar na porta 3000
