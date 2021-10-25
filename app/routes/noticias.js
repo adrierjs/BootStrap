24d15cf5f92a1bb991191201156bd6f722e1439d
adrierjs <adrierj@gmail.com>
2021-10-25 18:45:33 -0300

    views_com_js

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,7 +1,17 @@
 module.exports = function(app){
-    
     app.get('/noticias', function(req,res){
-        res.render('noticias/noticias');
-    });
+        var mysql = require('mysql');
+    //conexão com o servidor
+        var connection = mysql.createConnection({
+            host  : 'localhost',
+            user : 'root',
+            password : '1234',
+            database : 'portal_noticias'
+        });
+        
+        connection.query('select *from noticias', function(error, result) {
+            res.render("noticias/noticias", {noticias : result});
+        });
+     });
 
-};
+    };
