dd5698f34faff87434cde8c870213b40e27b6a94
adrierjs <adrierj@gmail.com>
2021-10-26 17:05:37 -0300

    consign

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 var app = require('./config/server');
 
-var rotaNoticias = require ('./app/routes/noticias');
-rotaNoticias(app);
+//var rotaNoticias = require ('./app/routes/noticias');
+//rotaNoticias(app);
 
-var rotaHome = require('./app/routes/home');
-rotaHome(app);
+//var rotaHome = require('./app/routes/home');
+//rotaHome(app);
 
-var rotaFormulario = require('./app/routes/formulario_inclusao_noticia');
-rotaFormulario(app);
+//var rotaFormulario = require('./app/routes/formulario_inclusao_noticia');
+//rotaFormulario(app); 
 
 app.listen(3000, function(){
     console.log('Servidor ON');
-});
+}); 
 
 //p o server iniciar, apertar control+alt+n
 //npm install -g nodemon // utilitário para salvar automaticamente as alterações sem precisar resetar o servidor 
--- config/server.js
+++ config/server.js
@@ -1,6 +1,10 @@
 var express = require('express'); //recuperando a biblioteca do express
+var consign = require('consign');
+
 var app = express();
 app.set('view engine','ejs');
 app.set('views','./app/views');
+
+consign().include('app/routes').into(app);
 
 module.exports = app;
