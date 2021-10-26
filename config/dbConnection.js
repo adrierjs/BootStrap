419ca5514a3f9693b6717668d85f4b1d7f5135e2
adrierjs <adrierj@gmail.com>
2021-10-26 17:26:10 -0300

    Refactoring-do portal-de-notícias-com-Consign(BD)

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,10 +1,18 @@
 var mysql = require('mysql');
         //conexão com o servidor
-module.exports = function (){
-       return mysql.createConnection({
-        host  : 'localhost',
-        user : 'root',
-        password : '1234',
-        database : 'portal_noticias'
+var connyMySQL = function(){
+    console.log('Conexao com bd foi estabelecida');
+    return mysql.createConnection({
+     host  : 'localhost',
+     user : 'root',
+     password : '1234',
+     database : 'portal_noticias'
+
     });
+}
+
+module.exports = function (){
+    console.log('O autoload carregou o módulo de conexão com o db')
+    return connyMySQL;
+   
 }
