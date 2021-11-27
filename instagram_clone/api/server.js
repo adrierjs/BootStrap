e128fbc610f938eebc65e69517c71b056c0ce279
adrierjs <adrierj@gmail.com>
2021-11-27 16:19:40 -0300

    incluindo_documentos_via_mongodb

--- instagram_clone/api/server.js
+++ instagram_clone/api/server.js
@@ -11,8 +11,34 @@ var port = 8080;
 
 app.listen(port);
 
+var db = new mongodb.Db(
+    'instagram',
+    new mongodb.Server('localhost', 27017, {}),
+    {}
+);
+
 console.log('Servidor HTP esta escutando na porta ' + port);
 
 app.get('/', function(req, res){
     res.send({msg:'Olá'});
+});
+
+
+app.post('/api', function(req, res){
+
+    var dados = req.body;
+
+    db.open(function(err, mongoclient){
+        mongoclient.collection('postagens', function(err, collection){
+            collection.insert(dados, function(err, records){
+                if(err){
+                    res.json({'status':'erro'});
+                } else{
+                    res.json({'status':'inclusao realizada com sucesso'});
+                }
+                mongoclient.close();
+            });
+        });
+    });
+
 });
