569d73947d9546be704b9d0f62198d11584fe520
adrierjs <adrierj@gmail.com>
2022-01-05 01:37:01 -0300

    basico_js

---
+++ JavaScript_basico~avan/const&&variaveis.js
@@ -0,0 +1,6 @@
+const valorIngressoAdulto = 20; //cons é uma constante, ou seja nao pode ter alteração
+let valorIngressoCrianca = 10;//let é uma variavel, sofre alteracao
+
+console.log("Ingresso adulto:"+valorIngressoAdulto);
+console.log("Ingresso criança: "+valorIngressoCrianca);
+
---
+++ JavaScript_basico~avan/tiposPrimitivos.js
@@ -0,0 +1,22 @@
+let nome = "Adrier"; //string literal
+
+let idade = 25; //number literal
+let bool = true;
+
+let sobrenome;//variavel indefinida
+let corSelecionado = null;//variavel nula
+
+//let pessoa = {}; //criar objeto vazio
+
+let pessoa = {
+    nome, idade, sobrenome
+   
+}
+
+pessoa.nome = "João"
+pessoa.idade = 19;
+pessoa.sobrenome = "Silva";
+
+
+console.log(pessoa);
+
